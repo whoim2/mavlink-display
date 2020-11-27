352166c69d244744d6b937dd5ef6b093937fc8b3
whoim2 <whoim@mail.ru>
2020-11-27 04:30:21 +0300

    Update mavlink-display.ino

--- mavlink-display.ino
+++ mavlink-display.ino
@@ -9,7 +9,7 @@
 SSD1306AsciiWire oled;
 
 #define MAV_TIMEOUT 5000 //mavlink timeout
-#define SERIAL_SPEED 9600 //mavlink input baud
+#define SERIAL_SPEED 57600 //mavlink input baud, 4800 for qczek 2.10
 //#define DEBUG
 mavlink_message_t msg;
 mavlink_status_t status;
